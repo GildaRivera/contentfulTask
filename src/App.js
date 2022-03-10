500ae4a465456f9787140b2f9952606cd382f603
gildarivera <gilda.rivera@telus.com>
2022-03-10 12:18:51 -0600

    Feat: getting all entries

--- src/App.js
+++ src/App.js
@@ -2,31 +2,36 @@ import './App.css';
 
 import EntryComponent from './EntryComponent/EntryComponent';
 import EntryFormComponent from './EntryFormComponent/EntryFormComponent';
-
-import { useState } from 'react'
-const array = [
-  { title: "Blog 1", content:"Contenido 1", author:"Javier Cabrera"},
-  { title: "Blog 2", content:"Contenido 2", author:"Javier Cabrera"},
-  { title: "Blog 3", content:"Contenido 3", author:"Javier Cabrera"},
-  { title: "Blog 4", content:"Contenido 4", author:"Javier Cabrera"},
-  { title: "Blog 5", content:"Contenido 5", author:"Javier Cabrera"},
-  { title: "Blog 6", content:"Contenido 6", author:"Javier Cabrera"},
-]
+import client from './contentful/connect';
+import { useEffect, useState } from 'react'
+const array = []
 
 function App() {
   const [entries, setEntries] = useState(array)
 
   const addNewEntry = (entry) => {
-    const newEntries = [...entries, entry]
-    setEntries(newEntries)
+    setEntries((prev)=>{
+      const newEntries = [...prev, entry]    
+      return newEntries
+    })
   }
+  //Use efect to fetch the entries
+  useEffect(()=>{
+    client.getEntries().then( (entries)=> {
+      entries.items.forEach((entry) =>{
+      entry.fields["id"]=entry.sys.id
+       addNewEntry(entry.fields)
+      });
+    });
+  }, [])
+
 
   return (
     <div>
       <EntryFormComponent postNewEntry={addNewEntry} />
       {
         entries.map(element => (
-          <EntryComponent title={element.title} content={element.content} author={element.author} />
+          <EntryComponent title={element.title} content={element.content} author={element.author} key={element.id}/>
         ))
       }
     </div>
--- src/contentful/connect.js
+++ src/contentful/connect.js
@@ -3,17 +3,17 @@ var client = contentful.createClient({
   space: '6zqckf2vka6k',
   accessToken: 'jWAkYQB9nep0fR5bkJgpti-1xZpio9ITJO7rtqJKrMU',
 });
-
-client.getEntries().then(function (entries) {
-  //console.log(entries)
-  // log the title for all the entries that have it
-  entries.items.forEach(function (entry) {
-    console.log(entry)
-    if (entry.fields.productName) {
-      console.log(entry.fields.productName);
-    }
-  });
-});
+export default client
+// client.getEntries().then(function (entries) {
+//   //console.log(entries)
+//   // log the title for all the entries that have it
+//   entries.items.forEach(function (entry) {
+//     console.log(entry)
+//     if (entry.fields.productName) {
+//       console.log(entry.fields.productName);
+//     }
+//   });
+// });
 
 // client.getEntry('api').then(function (entry) {
 //   // logs the entry metadata
